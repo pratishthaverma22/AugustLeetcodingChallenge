9dcd4a96a916f1c75aec3d81c0d019f973c3943c
pratishthaverma22 <30427779+pratishthaverma22@users.noreply.github.com>
2020-08-03 02:00:12 +0530

    Create DesignHashSet.cpp

---
+++ Week 1/DesignHashSet.cpp
@@ -0,0 +1,38 @@
+/*
+Design a HashSet without using any built-in hash table libraries.
+To be specific, your design should include these functions:
+
+add(value): Insert a value into the HashSet. 
+contains(value) : Return whether the value exists in the HashSet or not.
+remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
+
+Example:
+MyHashSet hashSet = new MyHashSet();
+hashSet.add(1);         
+hashSet.add(2);         
+hashSet.contains(1);    // returns true
+hashSet.contains(3);    // returns false (not found)
+hashSet.add(2);          
+hashSet.contains(2);    // returns true
+hashSet.remove(2);          
+hashSet.contains(2);    // returns false (already removed)
+*/
+
+
+class MyHashSet {
+public:
+    bool arr[1000000] {false};
+    MyHashSet() {
+    }
+    
+    void add(int key) {
+        arr[key] = true;
+    }
+    
+    void remove(int key) {
+        arr[key] = false;
+    }
+    bool contains(int key) {
+        return arr[key];
+    }
+};
