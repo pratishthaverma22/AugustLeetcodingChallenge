7d1c608d98f0e57c833f47632efffc37059a93b5
pratishthaverma22 <30427779+pratishthaverma22@users.noreply.github.com>
2020-08-14 09:54:49 +0530

    Create H-Index.cpp

---
+++ Week 2/H-Index.cpp
@@ -0,0 +1,31 @@
+/*
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [3,0,6,1,5]
+Output: 3 
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had 
+             received 3, 0, 6, 1, 5 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+*/
+
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        // 3 support variables for us
+        int s = 0, e = c.size() - 1, avg;
+        // the base of every happy binary search ever: having a sorted dataset
+        sort(begin(c), end(c));
+        // some good old binary search here to find the maximum element meeting the conditions
+        while (s <= e) {
+            if (c[avg = (e + s) / 2] < c.size() - avg) s = avg + 1;
+            else e = avg - 1;
+        }
+        return c.size() - s;
+    }
+};
